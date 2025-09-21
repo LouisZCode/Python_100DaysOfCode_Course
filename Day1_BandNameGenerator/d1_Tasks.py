a4fe3d59850e7b35518893be7c82590a99304d7e
LouisZCode <lgzg90@hotmail.com>
2025-09-21 14:40:56 +0200

    restart of quick Python review, did Day 1, to check Git and Github Modules

---
+++ Day1_BandNameGenerator/d1_Tasks.py
@@ -0,0 +1,11 @@
+#Printing, see what something is in the terminal
+print('Hello World!\nGoodbye World\n')
+
+#Careful with the variable names! 
+variable = input('Add the input here:\n')
+#print(variable)
+
+print('\n')
+#len helps you get the length of a dataset, like here, a string.
+print(f"The length of your variable is: {len(variable)}\n")
+#notice how we used the f-string, meaning,  f"" will let you add parameters insite using {}
---
+++ Day1_BandNameGenerator/d1_project.py
@@ -0,0 +1,9 @@
+#Band name generator. Make the user input the Name and the name of his/her pet, and combine them to create the Bands name
+print('Welcome to the Band name generator!')
+print("------------------------------------")
+print('What is your name?')
+name = input()
+print('Nice name!\nwhat is your pets name?')
+pet_name = input()
+print('Cool pets name!\nYour band name is:')
+print(name + " " + pet_name)
